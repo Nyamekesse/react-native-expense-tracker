271e863b19f5c865b0a028e3e8e99298b17d00d8
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-23 23:14:46 +0000

    added a fallback text to display empty if there are no expense lists

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,89 +1,18 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { GlobalStyles } from '../../constants/styles';
 import ExpensesList from './ExpensesList';
 import ExpensesSummary from './ExpensesSummary';
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    description: 'A pair of shoes',
-    amount: 56.4,
-    date: new Date('2024-12-19'),
-  },
-  {
-    id: 'e2',
-    description: 'A pair of trousers',
-    amount: 6.4,
-    date: new Date('2024-03-14'),
-  },
-  {
-    id: 'e3',
-    description: 'A bag',
-    amount: 36.4,
-    date: new Date('2024-04-28'),
-  },
-  {
-    id: 'e4',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
-  },
-  {
-    id: 'e5',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
-  },
-  {
-    id: 'e6',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
-  },
-  {
-    id: 'e7',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
-  },
-  {
-    id: 'e8',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
-  },
-  {
-    id: 'e9',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
-  },
-  {
-    id: 'e10',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
-  },
-  {
-    id: 'e11',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
-  },
-  {
-    id: 'e12',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
-  },
-];
-
-const ExpensesOutput = ({ expenses, expensesPeriod }) => {
+const ExpensesOutput = ({ expenses, expensesPeriod, fallBack }) => {
+  let content = <Text style={styles.infoText}>{fallBack}</Text>;
+  if (expenses.length > 0) {
+    content = <ExpensesList expenses={expenses} />;
+  }
   return (
     <View style={styles.container}>
-      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
-      <ExpensesList expenses={DUMMY_EXPENSES} />
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      {content}
     </View>
   );
 };
@@ -95,5 +24,11 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary700,
+  },
+  infoText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 32,
   },
 });
