a74fb0bc4fa264d4d8e8cd44c4045679645640dd
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-26 16:00:17 +0000

    refactored code solved update function not working

--- store/expenses-context.js
+++ store/expenses-context.js
@@ -4,74 +4,56 @@ const DUMMY_EXPENSES = [
   {
     id: 'e1',
     description: 'A pair of shoes',
-    amount: 56.4,
-    date: new Date('2024-12-19'),
+    amount: 59.99,
+    date: new Date('2021-12-19'),
   },
   {
     id: 'e2',
     description: 'A pair of trousers',
-    amount: 6.4,
-    date: new Date('2024-03-14'),
+    amount: 89.29,
+    date: new Date('2022-01-05'),
   },
   {
     id: 'e3',
-    description: 'A bag',
-    amount: 36.4,
-    date: new Date('2024-04-28'),
+    description: 'Some bananas',
+    amount: 5.99,
+    date: new Date('2021-12-01'),
   },
   {
     id: 'e4',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
+    description: 'A book',
+    amount: 14.99,
+    date: new Date('2022-02-19'),
   },
   {
     id: 'e5',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
+    description: 'Another book',
+    amount: 18.59,
+    date: new Date('2022-02-18'),
   },
   {
     id: 'e6',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
+    description: 'A pair of trousers',
+    amount: 89.29,
+    date: new Date('2022-01-05'),
   },
   {
     id: 'e7',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
+    description: 'Some bananas',
+    amount: 5.99,
+    date: new Date('2021-12-01'),
   },
   {
     id: 'e8',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
+    description: 'A book',
+    amount: 14.99,
+    date: new Date('2022-02-19'),
   },
   {
     id: 'e9',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
-  },
-  {
-    id: 'e10',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
-  },
-  {
-    id: 'e11',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
-  },
-  {
-    id: 'e12',
-    description: 'A Shirt',
-    amount: 16.4,
-    date: new Date('2024-12-19'),
+    description: 'Another book',
+    amount: 18.59,
+    date: new Date('2024-02-26'),
   },
 ];
 
@@ -92,10 +74,14 @@ function expensesReducer(state, action) {
         (expense) => expense.id === action.payload.id
       );
       const updatableExpense = state[updatableExpenseIndex];
-      const updatedItem = { ...updatableExpense, ...action.payload.data };
+      const updatedItem = {
+        ...updatableExpense,
+        ...action.payload.data,
+      };
       const updatedExpenses = [...state];
-      updatableExpense[updatableExpenseIndex] = updatedItem;
+      updatedExpenses[updatableExpenseIndex] = updatedItem;
       return updatedExpenses;
+
     case 'DELETE':
       return state.filter((expense) => expense.id !== action.payload);
 
@@ -113,8 +99,8 @@ function ExpensesContextProvider({ children }) {
   function deleteExpense(id) {
     dispatch({ type: 'DELETE', payload: id });
   }
-  function updateExpense(id, expensesData) {
-    dispatch({ type: 'UPDATE', payload: { id, expensesData } });
+  function updateExpense(id, expenseData) {
+    dispatch({ type: 'UPDATE', payload: { id: id, data: expenseData } });
   }
   const value = {
     expenses: expensesState,
