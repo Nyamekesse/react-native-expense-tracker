a75290d379a2bdd507dfd3dd32e805a19643d3ae
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-26 16:57:21 +0000

    added style to invalid labels and inputs if they are not valid

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { GlobalStyles } from '../../constants/styles';
 import { getFormattedDate } from '../../util/date';
 import Button from '../UI/Button';
 import Input from './Input';
@@ -71,6 +72,7 @@ const ExpenseForm = ({
         <Input
           style={styles.rowInput}
           label='Amount'
+          invalid={!inputs.amount.isValid}
           textInputConfig={{
             keyboardType: 'decimal-pad',
             onChangeText: inputChangedHandler.bind(this, 'amount'),
@@ -80,6 +82,7 @@ const ExpenseForm = ({
         <Input
           style={styles.rowInput}
           label='Date'
+          invalid={!inputs.date.isValid}
           textInputConfig={{
             placeholder: 'YYYY-MM-DD',
             maxLength: 10,
@@ -90,6 +93,7 @@ const ExpenseForm = ({
       </View>
       <Input
         label='Description'
+        invalid={!inputs.description.isValid}
         textInputConfig={{
           multiline: true,
           onChangeText: inputChangedHandler.bind(this, 'description'),
@@ -97,7 +101,9 @@ const ExpenseForm = ({
         }}
       />
       {formIsInvalid && (
-        <Text>Invalid input values please check your entered data</Text>
+        <Text style={styles.errorText}>
+          Invalid input values - please check your entered data
+        </Text>
       )}
       <View style={styles.buttons}>
         <Button style={styles.button} mode={'flat'} onPress={onCancel}>
@@ -136,5 +142,10 @@ const styles = StyleSheet.create({
   button: {
     minWidth: 120,
     marginHorizontal: 8,
+  },
+  errorText: {
+    textAlign: 'center',
+    color: GlobalStyles.colors.error500,
+    margin: 8,
   },
 });
